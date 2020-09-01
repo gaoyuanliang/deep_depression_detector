59db9300cc789ca4a9027301cd1bd9b03a80a39c
Jessica (Gaoyuan Liang) <gaoyuanliang@outlook.com>
2020-09-01 17:22:54 +0400

    Update jessica_deep_depression_dertector.py

--- jessica_deep_depression_dertector.py
+++ jessica_deep_depression_dertector.py
@@ -6,3 +6,22 @@ from tensorflow import keras
 from tensorflow.keras.backend import expand_dims
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+model = keras.models.load_model("depression.h5")
+
+def deep_depression_detector(activity_data_csv_file):
+	df = pandas.read_csv(activity_data_csv_file)
+	x = numpy.array([df['activity'].tolist()])
+	x = pad_sequences(
+		x, maxlen=65407, 
+		dtype='int32', padding='pre', truncating='pre',
+		value=0.0
+		)
+	x = expand_dims(x, axis=-1)
+	y_pred = model.predict(x)[0]
+	y_score = numpy.max(y_pred)
+	y_label = numpy.argmax(y_pred)
+	if y_label == 1:
+		return  {'prediction':'depressed', 'confidence': y_score}
+	else:
+		return  {'prediction':'nondepressed', 'confidence': y_score}
